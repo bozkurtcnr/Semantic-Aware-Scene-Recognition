d1c65e54055a0744e48f301c5ddabf72ef646330
bozkurtcnr <bozkurtcnr@gmail.com>
2020-09-17 16:28:19 +0300

    Update evaluation2.py

--- evaluation2.py
+++ evaluation2.py
@@ -25,6 +25,106 @@ import yaml
 parser = argparse.ArgumentParser(description='Semantic-Aware Scene Recognition Evaluation 2')
 parser.add_argument('--ConfigPath', metavar='DIR', help='Configuration file path')
 
+########################################################################################################################
+traindir = os.path.join(CONFIG['DATASET']['ROOT'], CONFIG['DATASET']['NAME'])
+valdir = os.path.join(CONFIG['DATASET']['ROOT'], CONFIG['DATASET']['NAME'])
+
+train_dataset = SUN397Dataset(traindir, "train")
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=CONFIG['VALIDATION']['BATCH_SIZE']['TRAIN'],
+                                               shuffle=False, num_workers=CONFIG['DATALOADER']['NUM_WORKERS'], pin_memory=True)
+
+val_dataset = SUN397Dataset(valdir, "val", tencrops=CONFIG['VALIDATION']['TEN_CROPS'])
+val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=CONFIG['VALIDATION']['BATCH_SIZE']['TEST'],
+                                             shuffle=False, num_workers=CONFIG['DATALOADER']['NUM_WORKERS'], pin_memory=True)
+
+classes = train_dataset.classes
+
+# Training Function
+
+def train(model,
+          optimizer,
+          criterion = nn.BCELoss(),
+          train_loader = train_loader,
+          valid_loader = val_loader,
+          num_epochs = 5,
+          eval_every = len(train_loader) // 2,
+          file_path = destination_folder,
+          best_valid_loss = float("Inf")):
+    
+    # initialize running values
+    running_loss = 0.0
+    valid_running_loss = 0.0
+    global_step = 0
+    train_loss_list = []
+    valid_loss_list = []
+    global_steps_list = []
+
+    # training loop
+    model.train()
+    for epoch in range(num_epochs):
+        for i, (mini_batch) in enumerate(dataloader):     
+            RGB_image = mini_batch['Image'].cuda()
+            semantic_mask = mini_batch['Semantic'].cuda()
+            semantic_scores = mini_batch['Semantic Scores'].cuda()
+            sceneLabelGT = mini_batch['Scene Index'].cuda()
+            
+            output = model(titletext, titletext_len)
+
+            loss = criterion(output, labels)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # update running values
+            running_loss += loss.item()
+            global_step += 1
+
+            # evaluation step
+            if global_step % eval_every == 0:
+                model.eval()
+                with torch.no_grad():                    
+                  # validation loop
+                  for (labels, (title, title_len), (text, text_len), (titletext, titletext_len)), _ in valid_loader:
+                      labels = labels.to(device)
+                      titletext = titletext.to(device)
+                      titletext_len = titletext_len.to(device)
+                      output = model(titletext, titletext_len)
+
+                      loss = criterion(output, labels)
+                      valid_running_loss += loss.item()
+
+                # evaluation
+                average_train_loss = running_loss / eval_every
+                average_valid_loss = valid_running_loss / len(valid_loader)
+                train_loss_list.append(average_train_loss)
+                valid_loss_list.append(average_valid_loss)
+                global_steps_list.append(global_step)
+
+                # resetting running values
+                running_loss = 0.0                
+                valid_running_loss = 0.0
+                model.train()
+
+                # print progress
+                print('Epoch [{}/{}], Step [{}/{}], Train Loss: {:.4f}, Valid Loss: {:.4f}'
+                      .format(epoch+1, num_epochs, global_step, num_epochs*len(train_loader),
+                              average_train_loss, average_valid_loss))
+                
+                # checkpoint
+                if best_valid_loss > average_valid_loss:
+                    best_valid_loss = average_valid_loss
+                    save_checkpoint(file_path + '/model.pt', model, optimizer, best_valid_loss)
+                    save_metrics(file_path + '/metrics.pt', train_loss_list, valid_loss_list, global_steps_list)
+    
+    save_metrics(file_path + '/metrics.pt', train_loss_list, valid_loss_list, global_steps_list)
+    print('Finished Training!')
+
+
+model = LSTM().to(device)
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+train(model=model, optimizer=optimizer, num_epochs=10)
+########################################################################################################################
 
 def evaluationDataLoader(dataloader, model, set):
     batch_time = utils.AverageMeter()
